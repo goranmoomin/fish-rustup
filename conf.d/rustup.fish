e7aaee356944db7f7d6b34ad6dcc49d761d2936d
Sungbin Jo <pcr910303@icloud.com>
2019-03-16 13:23:41 +0900

    Add uninstall event handler

--- conf.d/rustup.fish
+++ conf.d/rustup.fish
@@ -2,3 +2,11 @@ if type -q $HOME/.cargo/bin/rustup
     set PATH $HOME/.cargo/bin $PATH
     source (rustup completions fish | psub)
 end
+
+set -l name (basename (status -f) .fish){_uninstall}
+
+function $name --on-event $name
+    if set -l index (contains -i $HOME/.cargo/bin $PATH)
+        set -e PATH[$index]
+    end
+end
